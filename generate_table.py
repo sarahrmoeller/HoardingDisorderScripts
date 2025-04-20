70150d53eb4425c464ecd3d90004129c90f00f31
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-04-20 18:21:55 -0400

    replace main loop over project json files with map operation

--- generate_table.py
+++ generate_table.py
@@ -126,32 +126,22 @@ if __name__ == "__main__":
     project_dirs.remove('.gitignore')
 
     for project_dir in project_dirs:
-        # Only looking in the REVIEW directory since this should contain all documents
-        # with labels that have been agreed upon
+        # Only looking in the REVIEW directory, as this directory contains all
+        # adjudicated documents (and not documents with labels from only one 
+        # person)
         review_dir = f"./data/{project_dir}/REVIEW/"
+        json_files = os.listdir(review_dir)
 
-        # Get all .json files in REVIEW dir
-        json_files = [
-            f for f in os.listdir(review_dir) if os.path.splitext(f)[1] == '.json'
-        ]
-
-        project_rows = [dict()] * len(json_files) # List of rows in the csv file 
-
-        # This loop populates the rows list---we loop over an index so that we 
-        # don't need to create a new list in memory after every 
-        # .append method
-        for i in range(len(json_files)):
-            file = json_files[i]
-
-            doc = Document(path=review_dir + file)
-            project_rows[i] = {
-                'Project' : project_dir,
-                'Document Name' : doc.name, 
-                'Hoarder Flag' : doc.hoarder_flag,
-                **doc.label_counts,
-                'TTR' : doc.type_token_ratio,
-                'ASL' : doc.average_sentence_length,
-            }
+        project_docs = [Document(review_dir + file) for file in json_files]
+        # List of rows in the csv file 
+        project_rows = list(map(lambda doc: { 
+            'Project' : project_dir,
+            'Document Name' : doc.name, 
+            'Hoarder Flag' : doc.hoarder_flag,
+            **doc.label_counts,
+            'TTR' : doc.type_token_ratio,
+            'ASL' : doc.average_sentence_length,
+        }, project_docs))
         table_rows.extend(project_rows)
 
     df = pd.DataFrame(table_rows)
