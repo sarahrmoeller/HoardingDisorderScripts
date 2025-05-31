457730b54b2a5de4c62b8a3c7e624fa38c3cfe27
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-05-31 13:57:28 -0400

    Update generate_table.py logic to use `data` object

--- generate_table.py
+++ generate_table.py
@@ -1,31 +1,23 @@
-import os
 import pandas as pd
 from tqdm import tqdm
-from utils.document import Document
+from utils.datasaur import data
+
 
 if __name__ == "__main__":
     table_rows = []
-    project_dirs = os.listdir('./data')
-    project_dirs.remove('.gitignore')
-
-    for project_dir in project_dirs:
-        # Only looking in the REVIEW directory, as this directory contains all
-        # adjudicated documents (and not documents with labels from only one 
-        # person)
-        review_dir = f"./data/{project_dir}/REVIEW/"
-        json_files = os.listdir(review_dir)
-
-        project_docs = [Document(review_dir + file) 
-                        for file in tqdm(json_files, desc=project_dir)]
+    
+    pbar = tqdm(data.items())
+    for project, docs in pbar:
+        pbar.set_description(project)
         # List of rows in the csv file 
         project_rows = list(map(lambda doc: { 
-            'Project' : project_dir,
+            'Project' : project,
             'Document Name' : doc.name, 
             'Hoarder Flag' : doc.hoarder_flag,
             **doc.label_counts,
             'TTR' : doc.type_token_ratio,
             'ASL' : doc.average_sentence_length,
-        }, project_docs))
+        }, docs))
         table_rows.extend(project_rows)
 
     df = pd.DataFrame(table_rows)
