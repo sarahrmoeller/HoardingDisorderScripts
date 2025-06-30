0624fb5290db4bec97d2f0eb6201659ba492bb90
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-06-30 14:09:38 -0400

    organize document logistic regression models

--- log-reg-doc.r
+++ log-reg-doc.r
@@ -1,29 +1,31 @@
 all.data <- read.csv("./out/document_table.csv")
-all.data <- all.data[, !(names(all.data) %in% c("Project", "Document.Name"))] # Remove project and document names
-names(all.data)
+# Remove project and document names
+data <- all.data[, !(names(all.data) %in% c("Project", "Document.Name"))]
+names(data)
 
-# Simple regression on total labels per document
-total.slr <- glm(Hoarder.Flag ~ Total, data = all.data, family = "binomial")
-summary(total.slr)
+# Get all columns that we care about:
+total_rows <- names(data)[grepl("Total", names(data))]
+columns_list <- list("Total",
+                     total_rows,
+                     names(data)[!(names(data) %in%
+                                     c(total_rows, "ASL", "TTR",
+                                       "Hoarder.Flag"))],
+                     names(data)[grepl("Participant", names(data))])
+names(columns_list) <- c("Total",
+                         "Labels by Total",
+                         "Labels by Speaker, with ASL and TTR",
+                         "Participant-Only Labels")
 
-# Regression model by speaker
-total.rows <- names(all.data)[grepl("Total", names(all.data))]
-labels.by.speaker <- all.data[, !(names(all.data) %in% total.rows)]
-names(labels.by.speaker)
-speaker.label.mlr <- glm(Hoarder.Flag ~ . - TTR - ASL, data = labels.by.speaker, family = "binomial")
-speaker.label.part.mlr <- glm(Hoarder.Flag ~ . - TTR - ASL, data = labels.by.speaker[, !grepl("Interviewer", names(labels.by.speaker))],
-                              family = "binomial")
-speaker.mlr <- glm(Hoarder.Flag ~ ., data = labels.by.speaker, family = "binomial")
-summary(speaker.mlr)
-summary(speaker.label.mlr)
-summary(speaker.label.part.mlr)
+# Create a model for each set of columns
+mdls <- setNames(
+  lapply(columns_list, function(cols) {
+    glm(Hoarder.Flag ~ ., data = data[, c("Hoarder.Flag", cols)],
+        family = "binomial")
+  }),
+  names(columns_list)
+)
+lapply(mdls, summary)
 
-# Sanity check: if we regress on the total like this, we should get the same coefficients
-total.rows <- total.rows[!total.rows == "Total"] # don't include the Total column
-labels.by.total <- all.data[, c("Hoarder.Flag", total.rows)]
-total.mlr <- glm(Hoarder.Flag ~ ., data = labels.by.total, family = "binomial")
-summary(total.mlr)
-
-write.table(all.data[, c("Total", "Hoarder.Flag")],
+write.table(data[, c("Total", "Hoarder.Flag")],
             row.names = FALSE, sep = "\t",
             file = "./out/total_labels.tsv")
