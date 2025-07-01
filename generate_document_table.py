afb09c87440a4492885caf666180b186b8c99b20
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-07-01 00:07:21 -0400

    Improve progress bar and help redundancy

--- generate_document_table.py
+++ generate_document_table.py
@@ -9,34 +9,35 @@ if __name__ == "__main__":
     
     pbar = tqdm(data.by_project.items())
     for project, docs in pbar:
-        pbar.set_description(project)
         # List of rows in the csv file 
-        project_rows = list(map(lambda doc: { 
-            'Project' : project,
-            'Document Name' : doc.name, 
-            'Hoarder Flag' : doc.hoarder_flag,
-            **doc.label_counts,
-            'TTR-Interviewer' : ling.type_token_ratio(doc.tokens("Interviewer",
-                                                                 flat=True)), # type: ignore
-            'TTR-Participant' : ling.type_token_ratio(doc.tokens("Participant",
-                                                                 flat=True)), # type: ignore
-            'TTR-sent-Interviewer' : ling.type_token_ratio(doc.tokens(
-                                                                "Interviewer",
-                                                                flat=True),
-                                                           per_sent=True), # type: ignore
-            'TTR-sent-Participant' : ling.type_token_ratio(doc.tokens(
-                                                                 "Participant",
-                                                                 flat=True),
-                                                           per_sent=True), # type: ignore
-            'ASL-Interviewer' : ling.average_sentence_length(
-                doc.tokens("Participant")), # type: ignore
-            'ASL-Participant' : ling.average_sentence_length(
-                doc.tokens("Interviewer")), # type: ignore
-            'NP-counts-Interviewer' : ling.get_np_counts(doc.content_by_speaker("Interviewer")),
-            'NP-counts-Participant' : ling.get_np_counts(doc.content_by_speaker("Participant")),
-            'NP-ratio-Interviewer' : ling.get_np_ratios(doc.content_by_speaker("Participant")),
-            'NP-ratio-Participant' : ling.get_np_ratios(doc.content_by_speaker("Interviewer"))
-        }, docs))
+        project_rows = []
+        for doc in docs:
+            pbar.set_description(f"{doc}")
+
+            interviewer_tokens = doc.tokens("Interviewer", flat=True) # type: ignore
+            participant_tokens = doc.tokens("Participant", flat=True) # type: ignore
+
+            interviewer_nlp = ling.nlp(doc.content_by_speaker("Interviewer"))
+            participant_nlp = ling.nlp(doc.content_by_speaker("Participant"))
+
+            row = { 
+                'Project' : project,
+                'Document Name' : doc.name, 
+                'Hoarder Flag' : doc.hoarder_flag,
+                **doc.label_counts,
+                'TTR-Interviewer' : ling.type_token_ratio(interviewer_tokens),
+                'TTR-Participant' : ling.type_token_ratio(participant_tokens),
+                'TTR-sent-Interviewer' : ling.type_token_ratio(interviewer_tokens),
+                'TTR-sent-Participant' : ling.type_token_ratio(participant_tokens),
+                'ASL-Interviewer' : ling.average_sentence_length(
+                    doc.tokens("Participant")), # type: ignore
+                'ASL-Participant' : ling.average_sentence_length(
+                    doc.tokens("Interviewer")), # type: ignore
+                'NP-counts-Interviewer' : ling.get_np_counts(interviewer_nlp),
+                'NP-counts-Participant' : ling.get_np_counts(participant_nlp),
+                'NP-ratio-Interviewer' : ling.get_np_ratios(interviewer_nlp),
+                'NP-ratio-Participant' : ling.get_np_ratios(participant_nlp)
+            }
         table_rows.extend(project_rows)
 
     df = pd.DataFrame(table_rows)
