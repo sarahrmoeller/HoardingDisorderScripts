827279cf11125bd12c0e5a20478f7791383f9ef5
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-07-15 18:07:14 -0400

    Update log-reg-doc.r

--- log-reg-doc.r
+++ log-reg-doc.r
@@ -3,11 +3,15 @@ all.data <- read.csv("./out/document_table.csv")
 data <- all.data[, !(names(all.data) %in% c("Project", "Document.Name"))]
 names(data)
 
+# "Joining" CLF and SC columns
+data[["CLF.or.SC.Participant"]] <- (data[["Clarification.Participant"]] +
+                                      data[["Self.Correction.Participant"]])
+
 # Fix columns with lists in them instead of numbers
 pylist_to_vec <- function(l) {
   l <- gsub("\\[|\\]", "", l)  # Remove brackets
   l <- strsplit(l, ",")[[1]]   # Split by comma
-  as.numeric(trimws(l))         # Convert to numeric and trim whitespace
+  as.numeric(trimws(l))        # Convert to numeric and trim whitespace
 }
 
 for (col in names(data)[grepl("NP", names(data))]) {
@@ -32,28 +36,47 @@ labels <- c("Clarification", "Incomplete.Thought", "Misspeak",
             "Self.Correction", "Generic.Disfluency", "Overlap", "Unclear")
 
 # Get all columns that we care about:
-total_rows <- names(data)[grepl("Total", names(data))]
-extra_rows <- names(data)[grepl("TTR|ASL|NP", names(data))]
-label_rows <- names(data)[grepl(paste(labels, collapse = "|"),
-                                names(data)) & !grepl("Total", names(data))]
-columns_list <- list("Total" = "Total",
-                     "Labels by Speaker" = label_rows,
-                     "Non-Total Columns" =
-                       names(data)[!(names(data) %in%
-                                     c(total_rows, "Hoarder.Flag"))],
-                     "Participant Labels w/ extra" =
-                       names(data)[grepl("Participant", names(data))])
+total_cols <- names(data)[grepl("Total", names(data))]
+participant_cols <- names(data)[grepl("Participant", names(data))]
+# Removing Multicollinear columns
+# TTR.sent.Participant gives the same as TTR.Participant
+# We will investigate CLF.or.SC.Participant soon
+participant_cols <- participant_cols[!grepl("sent|CLF.or.SC.Participant",
+                                            participant_cols)]
+# Removing biased or unimportant labels
+biased_or_unimportant_columns <- c("Generic.Disfluency", "ASL", # Biased
+                                   "Misspeak", "Unclear") # Unimportant
+filtered_participant_cols <-
+  participant_cols[!grepl(paste(biased_or_unimportant_columns,
+                                collapse = "|"),
+                          participant_cols)]
+columns_list <- list(
+  "Total" = "Total",
+  "Participant Columns" = participant_cols,
+  "Filtered Participant Columns" = filtered_participant_cols,
+  "Using Combined CLF + SC" = c(filtered_participant_cols[!grepl("Clarification|Self.Correction", 
+                                                          filtered_participant_cols)], 
+                                "CLF.or.SC.Participant")
+)
 
 # Create a model for each set of columns
 mdls <- setNames(lapply(columns_list, function(cols) {
   glm(Hoarder.Flag ~ ., data = data[, c("Hoarder.Flag", cols)],
       family = "binomial")
 }), names(columns_list))
+mdls[["Interaction Term"]] <- glm(Hoarder.Flag ~ . + Clarification.Participant *
+                                    Self.Correction.Participant,
+                                  data = data[, c("Hoarder.Flag",
+                                                columns_list[[length(columns_list)]])],
+                                family = "binomial")
 lapply(mdls, summary)
 
-glm(Hoarder.Flag ~ ., data = data[, c("Hoarder.Flag", columns_list[[4]])],
-    family = "binomial")
+library(car)
+
+lapply(mdls[-1], vif) # Skip the total model for VIF
 
 write.table(data[, c("Total", "Hoarder.Flag")],
             row.names = FALSE, sep = "\t",
             file = "./out/total_labels.tsv")
+
+heatmap(cormat)
