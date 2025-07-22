8de9a04bac6de8098bbb7982e17cfc1794d5f8a9
Joseph Weaver <joseph@titaniumcoast.com>
2025-07-22 15:52:26 -0400

    Continued Work On Embeddings

--- generate_embedding.py
+++ generate_embedding.py
@@ -7,6 +7,8 @@ from utils.transcript import Transcript
 from sentence_transformers import SentenceTransformer
 from sqlitedict import SqliteDict
 
+import statistics as stats
+
 # Load the model
 model = SentenceTransformer(
     "Qwen/Qwen3-Embedding-8B",
@@ -27,24 +29,28 @@ if __name__ == "__main__":
     # List of rows in the csv file 
     sentence_pair_rows = []
     call_response_rows = []
+    call_response_per_transcript_rows = []
+    call_response_per_transcript_long_rows = []
 
     pbar = tqdm(data.by_transcript)
     for num in pbar:
+        call_response_transcript_rows = []
+
         tr = Transcript(num)
         
         pbar.set_description(f"{num}")
 
         lines = []
         for doc in tr.docs:
-            new_lines = [[doc.lines[i], doc._row_speakers_default[i]] for i in range(len(doc.lines))]
+            new_lines = [[doc.lines[i], doc._row_speakers_default[i], doc.name] for i in range(len(doc.lines))]
             # Remove Speaker Labels
-            new_lines = [[regexes.speaker_labels_restricted.sub('', line[0]), line[1]] for line in new_lines]
+            new_lines = [[regexes.speaker_labels_restricted.sub('', line[0]), line[1], line[2]] for line in new_lines]
             # Remove timestamps from the lines (and apply lowercase)
-            new_lines = [[regexes.timestamps.sub('', line[0]).strip().lower(), line[1]] for line in new_lines]
+            new_lines = [[regexes.timestamps.sub('', line[0]).strip().lower(), line[1], line[2]] for line in new_lines]
             # Replace bracketed stuff with better representations
-            new_lines = [[regexes.replace_tokens(line[0]), line[1]] for line in new_lines]
+            new_lines = [[regexes.replace_tokens(line[0]), line[1], line[2]] for line in new_lines]
             # Remove tokens that are not useful for us
-            new_lines = [[regexes.remove_tokens(line[0]), line[1]] for line in new_lines]
+            new_lines = [[regexes.remove_tokens(line[0]), line[1], line[2]] for line in new_lines]
             # Remove empty lines
             new_lines = [line for line in new_lines if line[0]]
 
@@ -79,15 +85,40 @@ if __name__ == "__main__":
             if lines[i][1] == 'Interviewer' and lines[i + 1][1] == 'Participant':
                 row = {
                     'Project': tr.number,
+                    'Document Name': lines[i][2],
                     'Hoarder Flag': tr.docs[0].hoarder_flag,  # Assuming all docs have the same hoarder flag
                     'Similarity': similarity[i][i + 1].item(),
                     'Call': lines[i][0],
                     'Response': lines[i + 1][0]
                 }
+                call_response_transcript_rows.append(row)
                 call_response_rows.append(row)
 
+        # OLD
         interviewer_tokens = doc.tokens("Interviewer", flat=True) # type: ignore
         participant_tokens = doc.tokens("Participant", flat=True) # type: ignore
+
+        mean = stats.mean([row["Similarity"] for row in call_response_transcript_rows])
+        std = stats.stdev([row["Similarity"] for row in call_response_transcript_rows])
+
+        mean_long = stats.mean([row["Similarity"] for row in call_response_transcript_rows if len(row["Call"]) > 10 and len(row["Response"]) > 10])
+        std_long = stats.stdev([row["Similarity"] for row in call_response_transcript_rows if len(row["Call"]) > 10 and len(row["Response"]) > 10])
+
+        row = {
+            'Project': tr.number,
+            'Hoarder Flag': tr.docs[0].hoarder_flag,
+            'Mean': mean,
+            'Std': std,
+        }
+        call_response_per_transcript_rows.append(row)
+
+        row = {
+            'Project': tr.number,
+            'Hoarder Flag': tr.docs[0].hoarder_flag,
+            'Mean': mean_long,
+            'Std': std_long,
+        }
+        call_response_per_transcript_long_rows.append(row)
 
         row = { 
             'Project' : doc.project,
@@ -113,3 +144,7 @@ if __name__ == "__main__":
     sp_df.to_csv('./out/embedding_sentence_pair_table.csv', index=False)
     cr_df = pd.DataFrame(call_response_rows)
     cr_df.to_csv('./out/embedding_call_response_table.csv', index=False)
+    cr_per_transcript_df = pd.DataFrame(call_response_per_transcript_rows)
+    cr_per_transcript_df.to_csv('./out/embedding_call_response_per_transcript_table.csv', index=False)
+    cr_per_transcript_long_df = pd.DataFrame(call_response_per_transcript_long_rows)
+    cr_per_transcript_long_df.to_csv('./out/embedding_call_response_per_transcript_long_table.csv', index=False)
