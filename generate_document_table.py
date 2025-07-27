641eb7e03b4250ecf1bbd831c781c170a1e69699
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-07-26 23:36:35 -0400

    Update generate_document_table.py to use new API correctly

--- generate_document_table.py
+++ generate_document_table.py
@@ -12,29 +12,24 @@ if __name__ == "__main__":
     for doc in pbar:
         pbar.set_description(f"{doc}")
 
-        interviewer_tokens = doc.tokens("Interviewer", flat=True) # type: ignore
-        participant_tokens = doc.tokens("Participant", flat=True) # type: ignore
-
-        interviewer_nlp = ling.nlp(doc.content_by_speaker("Interviewer"))
-        participant_nlp = ling.nlp(doc.content_by_speaker("Participant"))
+        sdoc_int = doc.stanza_doc("Interviewer")
+        sdoc_part = doc.stanza_doc("Participant")
 
         row = { 
             'Project' : doc.project,
             'Document Name' : doc.name, 
             'Hoarder Flag' : doc.hoarder_flag,
             **doc.label_counts,
-            'TTR-Interviewer' : ling.type_token_ratio(interviewer_tokens),
-            'TTR-Participant' : ling.type_token_ratio(participant_tokens),
-            'TTR-sent-Interviewer' : ling.type_token_ratio(interviewer_tokens),
-            'TTR-sent-Participant' : ling.type_token_ratio(participant_tokens),
+            'TTR-Interviewer' : ling.type_token_ratio(doc.tokens("Interviewer")),
+            'TTR-Participant' : ling.type_token_ratio(doc.tokens("Participant")),
             'ASL-Interviewer' : ling.average_sentence_length(
-                doc.tokens("Participant")), # type: ignore
-            'ASL-Participant' : ling.average_sentence_length(
                 doc.tokens("Interviewer")), # type: ignore
-            'NP-counts-Interviewer' : ling.get_np_counts(interviewer_nlp),
-            'NP-counts-Participant' : ling.get_np_counts(participant_nlp),
-            'NP-ratio-Interviewer' : ling.get_np_ratios(interviewer_nlp),
-            'NP-ratio-Participant' : ling.get_np_ratios(participant_nlp)
+            'ASL-Participant' : ling.average_sentence_length(
+                doc.tokens("Participant")), # type: ignore
+            'NP-counts-Interviewer' : ling.count_nps_in_sd(sdoc_int),
+            'NP-counts-Participant' : ling.count_nps_in_sd(sdoc_part), 
+            'NP-ratio-Interviewer'  : ling.NP_ratio_in_sd(sdoc_int),
+            'NP-ratio-Participant'  : ling.NP_ratio_in_sd(sdoc_part),
         }
         table_rows.append(row)
 
