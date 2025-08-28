b5d7769f61bbab54b84407e67ee552c33a436f35
Daniel A. Soto <dsotomail@gmail.com>
2025-08-28 13:50:01 -0400

    Adjust comment

--- utils/regexes.py
+++ utils/regexes.py
@@ -84,7 +84,7 @@ removable_token_patterns = [
     r'^(Interview )?\d{3}$', # Matches '001', 'Interview 001' (only if this is the whole line)
     r'PART \d of \d ENDS {ts}'.format(ts=timestamps.pattern),
     r'\[END OF RECORDING\]', # Matches literal '[END OF RECORDING]'
-    r'\(affirmative\)|\(negative\)' # Matches literal '[END OF RECORDING]'
+    r'\(affirmative\)|\(negative\)' # Remove (affirmative) and (negative)
 ]
 
 def remove_tokens(string: str) -> str:
