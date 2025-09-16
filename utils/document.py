139349bac9c7e4c647a37019db947eb8d3fd182e
Daniel A. Soto <dsotomail@gmail.com>
2025-09-16 00:45:45 -0400

    fix wrong 0-indexing in error message

--- utils/document.py
+++ utils/document.py
@@ -222,9 +222,10 @@ class Document:
                     other_speaker = self.speaker_tuple[current_speaker_ind-1]
                     row_speakers[:i] = [other_speaker] * i
                 else:
-                    warnings.warn(f'First {i} rows (out of {len(self.lines)}) '
-                                  f'empty in {self}, but there are too many '
-                                   'speakers! I don\'t know how to fill them.')
+                    warnings.warn(f'First {i+1} rows (out of '
+                                  f'{len(self.lines)}) empty in {self}, but '
+                                   'there are too many speakers! I don\'t '
+                                   'know how to fill them.')
         
         rows_without_speakers = tuple(i for i in range(len(row_speakers)) 
                                       if not row_speakers[i])
