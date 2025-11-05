4262d24b5e4808994f92c92739d316df1a541222
Daniel A. Soto <dsotomail@gmail.com>
2025-11-05 18:33:04 -0500

    Move utilities from fix_raw.py into separate utils/raw.py file

--- fix_raw.py
+++ fix_raw.py
@@ -4,6 +4,7 @@ document data, applying them all in order.
 """
 import os
 from collections import Counter
+from utils.raw import *
 
 
 """Step 0a: Remove non-txt files from raw data directories"""
@@ -16,21 +17,6 @@ for root, dirs, files in os.walk('./data/raw'):
 if '2001-003.txt' in os.listdir('./data/raw/set02'):
     os.rename('./data/raw/set02/2001-003.txt', 
             './data/raw/set02/2001_003.txt')
-
-"""Step 0c: Set up useful data structures, so accessing documents is easier."""
-doc_paths = [root + '/' + file
-             for root, dirs, files in os.walk('./data/raw')
-             for file in files if file.endswith('.txt')]
-doc_names = [os.path.basename(path) for path in doc_paths]
-transcript_numbers = set(map(
-    lambda doc_name: doc_name.split('_')[0], doc_names
-))
-docs_by_transcript = {tn: [dp for dp in doc_paths
-                           if os.path.basename(dp).startswith(tn)]
-                      for tn in transcript_numbers}
-def get_doc_path(doc_name: str) -> str:
-    set_num = '1' if doc_name[0] == '0' else doc_name[0]
-    return f'./data/raw/set0{set_num}/{doc_name}'
 
 
 """1. Fix transcript 012"""
---
+++ utils/raw.py
@@ -0,0 +1,17 @@
+"""Utilities for accessing raw document files."""
+import os
+
+
+doc_paths = [root + '/' + file
+             for root, dirs, files in os.walk('./data/raw')
+             for file in files if file.endswith('.txt')]
+doc_names = [os.path.basename(path) for path in doc_paths]
+transcript_numbers = set(map(
+    lambda doc_name: doc_name.split('_')[0], doc_names
+))
+docs_by_transcript = {tn: [dp for dp in doc_paths
+                           if os.path.basename(dp).startswith(tn)]
+                      for tn in transcript_numbers}
+def get_doc_path(doc_name: str) -> str:
+    set_num = '1' if doc_name[0] == '0' else doc_name[0]
+    return f'./data/raw/set0{set_num}/{doc_name}'
