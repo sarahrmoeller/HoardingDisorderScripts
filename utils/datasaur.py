ccdcc1ff11c9071fe8465e69eec95f7aab039b6b
Daniel A. Soto <dsotomail@gmail.com>
2025-11-18 18:57:19 -0500

    Move project functions from datasaur.py to separate file

--- utils/datasaur.py
+++ utils/datasaur.py
@@ -1,20 +1,7 @@
 from .document import Document
 import os
+from .project import projects, review_dir
 
-
-project_dir_locations = ('./data/mathews/documents/datasaur_exports/' + 
-                         'truncated_clauses')
-projects = [d for d in os.listdir(project_dir_locations)
-            if os.path.isdir(os.path.join(project_dir_locations, d))]
-
-def review_dir(project: str) -> str:
-    """
-    Given name of a project, return the path to the REVIEW directory
-    assuming current directory is the project's root.
-
-    Example: "HD_set1_1-7" -> "./{project_dir_locations}/HD_set1_1-7/REVIEW/"
-    """
-    return f"{project_dir_locations}/{project}/REVIEW/"
 
 by_project = {project: [Document(review_dir(project) + filename) 
                         for filename in os.listdir(review_dir(project))] 
