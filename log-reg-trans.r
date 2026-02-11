a1d3c044e2c13723ed20606951cdd4a3e5cbc8a0
Daniel A. Soto <dsotomail@gmail.com>
2026-02-11 16:52:48 -0500

    log-reg-trans.r: keep transcripts 2005 and 2008

--- log-reg-trans.r
+++ log-reg-trans.r
@@ -1,8 +1,4 @@
 all.data <- read.csv("./out/transcript_table.csv")
-# Remove transcripts 2005 and 2008 as their speaker detection is broken
-# right now
-all.data <- all.data[!(all.data$Transcript %in% c("2005", "2008")), ]
-all.data <- all.data[, names(all.data) != "Transcript"] # Remove Transcript column
 names(all.data)
 
 mdl <- glm(Hoarder.Flag ~ Incomplete.Thought.Participant +
