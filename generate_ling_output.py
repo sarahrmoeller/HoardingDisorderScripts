4fedd0d9335bf9da500baf8e0385baef30eab7d6
Daniel A. Soto <dsotomail@gmail.com>
2026-02-13 17:02:24 -0500

    adjust logic for when there is no participant content found in a document

--- generate_ling_output.py
+++ generate_ling_output.py
@@ -3,6 +3,7 @@ import utils.ling as ling
 import utils.raw as raw
 from utils.document import RawDocument
 from tqdm import tqdm
+import warnings
 
 
 if __name__ == "__main__":
@@ -10,18 +11,22 @@ if __name__ == "__main__":
 
     for doc_path in tqdm(raw.doc_paths):
         doc = RawDocument(doc_path)
-        stanza_doc = ling.nlp(doc.content("Participant"))
-        speaker_tokens = [[token.text for token in sent.tokens] 
-                          for sent in stanza_doc.sentences] # type: ignore
-        if speaker_tokens is None:
-            raise ValueError(f"Could not extract speaker tokens for document: "
-                             f"{doc.name}")
+        stanza_doc_part = ling.nlp(doc.content("Participant"))
+        participant_tokens = [[token.text for token in sent.tokens] 
+                          for sent in stanza_doc_part.sentences] # type: ignore
+        if not participant_tokens:
+            if doc.content("Interviewer"):
+                warnings.warn(f"No participant content found in {doc.name}" +
+                               "(Note: Interviewer content present)")
+            else:
+                raise ValueError(f"No content found in {doc.name} "
+                                  "for any speaker")
 
         csv_rows.append({
             'Document Name': doc.name,
-            'TTR': ling.type_token_ratio(speaker_tokens),
-            'ASL': ling.average_sentence_length(speaker_tokens),
-            'NPR': ling.NP_ratio_in_sd(stanza_doc)
+            'TTR': ling.type_token_ratio(participant_tokens),
+            'ASL': ling.average_sentence_length(participant_tokens),
+            'NPR': ling.NP_ratio_in_sd(stanza_doc_part)
         })
 
     df = pd.DataFrame(csv_rows)
