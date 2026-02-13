dbadc3492ed13dfbe95345b5a0380e1dc23f594b
Daniel A. Soto <dsotomail@gmail.com>
2026-02-13 16:04:04 -0500

    (incomplete) reorganize stanza.py

--- generate_ling_output.py
+++ generate_ling_output.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import utils.ling as ling
 import utils.raw as raw
+import utils.stanza as stnz
 from utils.document import RawDocument
 
 
@@ -9,8 +10,10 @@ if __name__ == "__main__":
 
     for doc_path in raw.doc_paths:
         doc = RawDocument(doc_path)
+        stanza_doc = ling.nlp(doc.content("Participant"))
+
         csv_rows.append({
             'Document Name': raw.get_doc_path(doc_path).split('/')[-1],
-            'TTR': ling.type_token_ratio(doc.lines("Participant")),
-            'ASL': ling.average_sentence_length(doc.lines("Participant"))
+            'TTR': ling.type_token_ratio(),
+            'ASL': ling.average_sentence_length()
         })
