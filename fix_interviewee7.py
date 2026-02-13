1da4662e22a3b83bccd68a042b23fa59b6f4f4ce
Daniel A. Soto <dsotomail@gmail.com>
2026-02-13 16:30:29 -0500

    Track new fix_interviewee7.py (that actually does what we want)

--- fix_interviewee7.py
+++ fix_interviewee7.py
@@ -6,7 +6,7 @@ from utils.document import RawDocument
 if __name__ == "__main__":
     for doc_path in raw.doc_paths:
         doc = RawDocument(doc_path)
-        if re.search(r'Interviewer\d', doc.content()):
-            print(f"Document {doc.name} has Interviewer\\d in content.") 
+        if re.search(r'Interviewee\d', doc.content()):
+            print(f"Document {doc.name} has Interviewee\\d in content.") 
             with open(doc_path, 'w') as f:
-                f.write(re.sub(r'Interviewer\d', 'Interviewer', doc.content()))
+                f.write(re.sub(r'Interviewee\d', 'Interviewee', doc.content()))
