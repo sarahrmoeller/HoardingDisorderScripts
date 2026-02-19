cbcd01b939908114b940880e52bcac16722d0978
Daniel A. Soto <dsotomail@gmail.com>
2026-02-19 11:44:48 -0500

    by_project -> docs_by_project

--- utils/data/datasaur.py
+++ utils/data/datasaur.py
@@ -3,8 +3,8 @@ import os
 from .project import projects, review_dir
 
 
-by_project = {project: [DatasaurDocument(review_dir(project) + filename) 
+docs_by_project = {project: [DatasaurDocument(review_dir(project) + filename) 
                         for filename in os.listdir(review_dir(project))] 
-              for project in projects}
+                   for project in projects}
 
-docs = [doc for doc_list in by_project.values() for doc in doc_list]
+docs = [doc for doc_list in docs_by_project.values() for doc in doc_list]
