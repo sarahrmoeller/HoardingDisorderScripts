251aa351b252b6d63241eec9bf571c5f181510fa
Daniel A. Soto <dsotomail@gmail.com>
2026-02-19 11:46:18 -0500

    Move contents in project.py to datasaur.py

--- utils/data/datasaur.py
+++ utils/data/datasaur.py
@@ -1,8 +1,23 @@
 from ..document import DatasaurDocument
 import os
-from .project import projects, review_dir
+
+# Functionality for projects
+project_dir_locations = ('./data/mathews/documents/datasaur_exports/' + 
+                         'truncated_clauses')
+projects = [d for d in os.listdir(project_dir_locations)
+            if os.path.isdir(os.path.join(project_dir_locations, d))]
+
+def review_dir(project: str) -> str:
+    """
+    Given name of a project, return the path to the REVIEW directory
+    assuming current directory is the project's root.
+
+    Example: "HD_set1_1-7" -> "./{project_dir_locations}/HD_set1_1-7/REVIEW/"
+    """
+    return f"{project_dir_locations}/{project}/REVIEW/"
 
 
+# Functionality for getting all datasaur documents
 docs_by_project = {project: [DatasaurDocument(review_dir(project) + filename) 
                         for filename in os.listdir(review_dir(project))] 
                    for project in projects}
