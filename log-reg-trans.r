97e2b3bbc36a3914e76f512b965dc81364cc5631
Daniel A. Soto <dsotomail@gmail.com>
2026-02-19 11:17:11 -0500

    Untrack log-reg-trans.r

--- log-reg-trans.r
+++
@@ -1,9 +0,0 @@
-all.data <- read.csv("./out/transcript_table.csv")
-names(all.data)
-
-mdl <- glm(Hoarder.Flag ~ Incomplete.Thought.Participant +
-             Clarification.Participant +
-             Overlap.Participant +
-             Self.Correction.Participant,
-           data = all.data, family = "binomial")
-summary(mdl)
