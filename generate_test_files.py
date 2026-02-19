3400bb3e6e1d3b7115d5d97786ff1afd31041313
Daniel A. Soto <dsotomail@gmail.com>
2026-02-19 10:46:01 -0500

    fix: write_to_file takes dir, not full path

--- generate_test_files.py
+++ generate_test_files.py
@@ -2,4 +2,4 @@ from tests.test_document import test_docs
 
 
 for doc in test_docs.values():
-    doc.write_to_file(f'tests/test_doc_contents/{doc.name}')
+    doc.write_to_file(f'./tests/test_doc_contents/')
