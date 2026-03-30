85a79f11cb2f6b8df8db4b8345ccf3a0011a1102
dsoto1219 <dsotomail1219@gmail.com>
2026-03-30 12:12:17 -0400

    Check for duplicates specifically without the same name

--- find_duplicates.py
+++ find_duplicates.py
@@ -6,4 +6,5 @@ from itertools import product
 raw_docs = [TextDocument(dp) for dp in raw.doc_paths]
 
 print([doc_pair for doc_pair in product(raw_docs, repeat=2)
-       if doc_pair[0].full_text == doc_pair[1].full_text])
+       if doc_pair[0].full_text == doc_pair[1].full_text and
+        doc_pair[0].name != doc_pair[1].name])
