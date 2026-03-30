d48a7f33fad45f5d9510501e88d99ae80ab6725c
dsoto1219 <dsotomail1219@gmail.com>
2026-03-30 12:12:53 -0400

    Use pprint instead of print

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,10 +1,11 @@
 import utils.data.raw as raw
 from utils.document import TextDocument
 from itertools import product
+from pprint import pprint
 
 
 raw_docs = [TextDocument(dp) for dp in raw.doc_paths]
 
-print([doc_pair for doc_pair in product(raw_docs, repeat=2)
-       if doc_pair[0].full_text == doc_pair[1].full_text and
-        doc_pair[0].name != doc_pair[1].name])
+pprint([doc_pair for doc_pair in product(raw_docs, repeat=2)
+        if doc_pair[0].full_text == doc_pair[1].full_text and
+           doc_pair[0].name != doc_pair[1].name])
